57e4fab5ad5f0f6a359a5909b6b618056ef44eee
Dorian-DV <ddorianvlad@yahoo.ro>
2023-12-18 19:49:24 +0200

    Update scripts.js used classes

--- scripts.js
+++ scripts.js
@@ -1,27 +1,111 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-let canvasWidth = 1400, canvasHeight = 500; // game canvas size for redraw - display size in html file
-//let canvasWidth = 3350, canvasHeight = 500; // size used for development
-let xPositionInPixelsBackground = 1100,  score = 0, gameOver = 0, executingJump = 0, gameSpeed = 27; // game speed starts at 27 (lowest) increased to 8 (highest)
-let dinosaurWidth = 50, dinosaurHeight = 120, crouching = 0;
-let obstacleWidth = 40;
+let canvasWidth = 1400, canvasHeight = 500; // game canvas size
+let xPositionInPixelsBackground = 1100,  score = 0, gameOver = 0, executingJump = 0, crouching = 0, gameSpeed = 27; // game speed starts at 27ms (lowest) increasing to 8ms (highest)
+let dinosaurWidth = 50, dinosaurHeight = 120;
 let upperLeftX = 170, upperLeftY_Initial = 320, upperLeftY = upperLeftY_Initial; // upper left coordinates for the game character
-let jumpID;	// jump interval
-let obstacles = []; //to be populated with x coordinates for the game obstacles
-let obstacleType = []; // type 1 = ground obstacle / type 2 = flying obstacle
-let obstacleGroupCount = []; // each obstacle is treated as a "group" of 1 to 3 objects - group counts are stored here
-let obstacleGapLengths = [1650, 2200, 2750, 3300]; // fixed values representing the four generation points of the obstacles - this randomizes distance between obstacles
-let groupMembersHeight = []; // each obstacle "group member" has 3 height levels - height factors of 1, 2 or 3 are stored here
-let obstacleTop = 390;
-let obstacleBottom = 440;
-let generationPoint = 1050; // distance from left side of canvas - once passed by the last obstacle triggers generation of new obstacle
-let obstacleFlyingHeight = 365;
-let flyingObstacleWidth = 120;
-let heightSegment = -45// to be multiplied by height factor in groupMembersHeight
+let obstacles = []; // to be populated with "Obstacle" objects
+let generationTrigger = 1050; // distance from the left side of canvas - once passed by the last obstacle triggers generation of new obstacle
+// animation intervals used:
+let jumpID;
 let scoreID;
 let moveObstaclesID;
 let moveBackgroundID = setInterval(moveBackground, gameSpeed);
-runGame();
+
+class Obstacle {
+	static #generationPoints = [1650, 2200, 2750, 3300]; // fixed values representing the four generation points of the obstacles - this randomizes distance between obstacles
+	static #heightSegment = -45// to be multiplied by height factor in #elementsHeight (factor can be 1, 2 or 3) => obstacles will be either 45, 90 or 135px in height
+	//(negative value needed because canvas origin is at the top left corner and we are drawing towards it)
+	static #obstacleTop = 395; //the top of the smallest obstacles is at 395px
+	static #obstacleBottom = this.#obstacleTop + this.#heightSegment * -1; // an obstacle segment is 45px high
+	#position;
+    #type;
+	
+    constructor(position, type) {
+		this.#position = position;
+		this.#type = type;
+    }
+	
+    getPosition() {
+		return this.#position;
+    }
+	
+    setPosition(newValue) {
+		this.#position = newValue;
+    }
+	
+    getType() {
+		return this.#type;
+    }
+	
+	getHeightSegment() {
+		return Obstacle.#heightSegment;
+	}
+
+	static getGenerationPoints() {
+		return Obstacle.#generationPoints[getRandomIntInclusive(0, 3)];
+	}
+
+	static getObstacleTop() {
+		return Obstacle.#obstacleTop;
+	}
+
+	static getObstacleBottom() {
+		return Obstacle.#obstacleBottom;
+	}
+}
+
+class GroundObstacle extends Obstacle {
+	static #obstacleWidth = 40;
+	static #groupGap = 5; // the gap between a ground obstacle's elements
+    #elementsCount = getRandomIntInclusive(1,3);
+    #elementsHeight = []; // array will have "#elementsCount" values of 1 to 3
+    
+    constructor(position) {
+        super(position, "ground");
+        this.setElementsHeight();
+    }
+    
+    setElementsHeight() {
+        for (let i = 0; i < this.#elementsCount; ++i) {
+			let heightValue = getRandomIntInclusive(1,3);
+			this.#elementsHeight.push(heightValue);
+        }
+    }
+	
+	getElementsHeight(elemIndex) {
+		return this.#elementsHeight[elemIndex];
+	}
+	
+	getObstacleWidth(){
+		return GroundObstacle.#obstacleWidth;
+	}
+
+	getElementsCount() {
+		return this.#elementsCount;
+	}
+
+	static getGroupGap() {
+		return GroundObstacle.#groupGap;
+	}
+}
+
+class FlyingObstacle extends Obstacle {
+    static #flyingHeight = 365;
+    static #obstacleWidth = 120;
+
+    constructor(position) {
+        super(position, "flying");
+    }
+
+	getFlyingHeight() {
+		return FlyingObstacle.#flyingHeight;
+	}
+
+	getObstacleWidth() {
+		return FlyingObstacle.#obstacleWidth;
+	}
+}
 
 function moveBackground() { // moves background image right to left in increments of 10px
 	document.getElementById('game').style.backgroundPositionX = xPositionInPixelsBackground + "px";
@@ -30,26 +114,22 @@ function moveBackground() { // moves background image right to left in increment
 	} else {
 		xPositionInPixelsBackground -= 10;
 	}
-	//console.log(xPositionInPixelsBackground);
 }
 
 function runGame() {
 	generateObstacles();
 	scoreID = setInterval(incrementScore, 1000);
-	//obstaclesID = setInterval(generateObstacles, 1200);
 	moveObstaclesID = setInterval(moveObstacles, gameSpeed);
 	document.addEventListener('keydown', moveDinosaur);
-	drawGame(upperLeftX, upperLeftY);
 }
 
 function incrementScore() {
 	++score;
 	document.getElementById("score").innerHTML = score.toString().padStart(4, '0');
-	if (score % 10 == 0) {
-		if (gameSpeed >= 9) {
+	if (score % 5 == 0) { // speed increases every 5 seconds
+		if (gameSpeed > 8) { // highest speed is 8ms
 			--gameSpeed;
-			//console.log(gameSpeed);
-			clearInterval(moveObstaclesID);
+			clearInterval(moveObstaclesID); // clear intervals and reset with updated speed value
 			moveObstaclesID = setInterval(moveObstacles, gameSpeed);
 			clearInterval(moveBackgroundID);
 			moveBackgroundID = setInterval(moveBackground, gameSpeed);
@@ -58,48 +138,35 @@ function incrementScore() {
 }
 
 function getRandomIntInclusive(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1) + min); //min-max inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum and minimum values are inclusive
 }
 
 function generateObstacles() {
-	let newX = obstacleGapLengths[getRandomIntInclusive(0, 3)];
-	obstacles.push(newX); 									// sets generation point for new obstacle
-	if (gameSpeed <= 15) { // 'flying' obstacles are generated starting at game speed 15
-		let setObstacleType = getRandomIntInclusive(1,10); 
-		if (setObstacleType < 9) { // assigns type to new obstacle (either 1 ground or 2 flying)
-			obstacleType.push(1);  // 80% of the cases a ground obstacle will be generated
-		} else {
-			obstacleType.push(2);		
-		}
-	} else {
-		obstacleType.push(1);
-	}
-	groupMembersHeight.push([]);							// creates new entry in the 'groupMembersHeight' array
-	if (obstacleType[obstacleType.length - 1] == 1) { // if current obstacle is "ground"
-		obstacleGroupCount.push(getRandomIntInclusive(1, 3));	// set no. of obstacles in group
-		groupCountLast = obstacleGroupCount[obstacleGroupCount.length - 1];
-		groupHeightLast = groupMembersHeight.length - 1; // last element of 'groupMembersHeight'
-		for (let i = 0; i < groupCountLast; ++i) { // populate groupMembersHeight with values 1 to 3
-			groupMembersHeight[groupHeightLast].push(getRandomIntInclusive(1, 3));
-		}
-	} else {
-		obstacleGroupCount.push(1); // group count for flying obstacles
-	}
+    let setObstacleType = getRandomIntInclusive(1,10);
+    let horizPosition = Obstacle.getGenerationPoints(); // selects a random value from the array at line 21 as the starting horizontal position (outside visible area)
+    let newObstacle;
+	if (gameSpeed <= 15 && setObstacleType > 8) { // 'flying' obstacles are generated starting at game speed 15 in 20% of cases 
+		newObstacle = new FlyingObstacle(horizPosition);	
+    } else {
+		newObstacle = new GroundObstacle(horizPosition);
+    }
+	obstacles.push(newObstacle);
 }
 
 function moveObstacles() {
 	for (let i = 0; i < obstacles.length; ++i) {
-		obstacles[i] -= 10;
+        let updatedPosition = obstacles[i].getPosition() - 10;
+        obstacles[i].setPosition(updatedPosition);
 	}
 	if (collision()) {
 		endGame();
 	}
-	if (obstacles[obstacles.length - 1] <= generationPoint) {
+	if (obstacles[obstacles.length - 1].getPosition() <= generationTrigger) { //when the last obstacle passes the generation point - another is generated
 		generateObstacles();
 	}
-	deleteObstacles();
+	deleteObstacles(); // objects that have passed the character and are outside the screen get deleted 
 	drawGame(upperLeftX, upperLeftY);
 }
 
@@ -115,8 +182,6 @@ function endGame() {
 
 function resetGame() {
 	obstacles = [];
-	obstacleGroupCount = [];
-	groupMembersHeight = [];
 	executingJump = 0;
 	gameSpeed = 27;
 	crouching = 0;
@@ -132,11 +197,8 @@ function resetGame() {
 
 function deleteObstacles() {
 	for (let i = 0; i < obstacles.length; ++i) {
-		if (obstacles[i] <= -150) {
-			obstacleType.splice(i, 1);
+		if (obstacles[i].getPosition() <= -150) {
 			obstacles.splice(i, 1);
-			obstacleGroupCount.splice(i, 1);
-			groupMembersHeight.splice(i, 1);
 		}
 	}
 }
@@ -168,7 +230,6 @@ function jump() {
 		} else {
 			jumpIncrement = 20;
 		}
-
 		if (!jumpPeaked) { // move towards the jump peak and - once reached - move away from it
 			upperLeftY -= jumpIncrement;
 		} else {
@@ -176,13 +237,11 @@ function jump() {
 		}
 		if (collision()) {
 			endGame();
-		
 		}
-		if (!jumpPeaked && upperLeftY <= jumpLimit) {
+		if (!jumpPeaked && upperLeftY <= jumpLimit) { //jump peaks if the character's top side reaches or passes the jumpLimit
 			jumpPeaked = 1;
 		}
-
-		if (jumpPeaked == 1 && upperLeftY >= upperLeftY_Initial) { // upperLeftY_Initial is at the top edge of the character - this instruction block ends the jump
+		if (jumpPeaked && upperLeftY >= upperLeftY_Initial) { // upperLeftY_Initial is at the top edge of the character at the groud position - this instruction block ends the jump
 			clearInterval(jumpID);
 			upperLeftY = upperLeftY_Initial;
 			executingJump = 0;
@@ -192,45 +251,52 @@ function jump() {
 			document.removeEventListener('keyup', ()=>{shortJump = 1});
 		}
 		drawGame(upperLeftX, upperLeftY);
-	}, 15);
+	}, 15); // 15ms is the interval for the jump animation
 }
 
 let heightWidthDifference = dinosaurHeight - dinosaurWidth;
-function crouch() { // crouch() and resetDifference
+function crouch() { // crouch works by flipping the width and height values of the character
 	crouching = 1;
-	dinosaurWidth = 50 + heightWidthDifference, dinosaurHeight = 120 - heightWidthDifference, upperLeftY = upperLeftY_Initial + heightWidthDifference;
+	dinosaurWidth = 50 + heightWidthDifference;
+	dinosaurHeight = 120 - heightWidthDifference;
+	upperLeftY = upperLeftY_Initial + heightWidthDifference;
 	document.addEventListener('keyup', crouchEnd);
 }
 
 function crouchEnd() {
 	document.removeEventListener('keyup', crouchEnd);
-	dinosaurWidth = 50, dinosaurHeight = 120, upperLeftY = upperLeftY_Initial;
+	dinosaurWidth = 50;
+	dinosaurHeight = 120;
+	upperLeftY = upperLeftY_Initial; // reset to initial values after crouch
 	crouching = 0;
 }
 
 function collision() {
-	let collisionPresent = 0;
-	for (let i = 0; i < obstacles.length; ++i) { // traverse all existing obstacles
-		if (!collisionPresent) {
-			let aX = upperLeftX, aY = upperLeftY + dinosaurHeight, bX = upperLeftX + dinosaurWidth, bY = upperLeftY + dinosaurHeight; // a and b are the two bottom corners of the "dinosaur", their coordinates are aX, aY, bX, bY
-			if (obstacleType[i] == 1) { // 1 => 'ground' obstacle type
-				let obstacleGroupGap = 0; // to be incremented by 45
-				for (let k = 0; k < groupMembersHeight[i].length; ++k) { // traverse each obstacle group member
-					let obstacleTopLeftX = obstacles[i] + obstacleGroupGap, obstacleTopLeftY = obstacleTop + 5 + ((groupMembersHeight[i][k] - 1) * heightSegment);
-					let obstacleTopRightX = obstacles[i] + obstacleGroupGap + obstacleWidth;
-					if ((aX > obstacleTopLeftX && aX < obstacleTopRightX && aY > obstacleTopLeftY) || (bX > obstacleTopLeftX && bX < obstacleTopRightX && bY > obstacleTopLeftY)) {
+    let collisionPresent = 0;
+    for (let i = 0; i < obstacles.length; ++i) { // traverse all existing obstacles
+        if (!collisionPresent) {
+            let aX = upperLeftX, aY = upperLeftY + dinosaurHeight, bX = upperLeftX + dinosaurWidth, bY = upperLeftY + dinosaurHeight; // a and b are the two bottom corners of the game character, their coordinates are aX, aY, bX, bY
+            if (obstacles[i].getType() == "ground") {
+                let drawingPointX = 0;
+                for (let k = 0; k < obstacles[i].getElementsCount(); ++k) { // traverse each obstacle group member
+                    let obstacleTopLeftX = obstacles[i].getPosition()+ drawingPointX, obstacleTopLeftY = Obstacle.getObstacleTop() + ((obstacles[i].getElementsHeight(k) - 1) * obstacles[i].getHeightSegment());
+					let obstacleTopRightX = obstacles[i].getPosition() + drawingPointX + obstacles[i].getObstacleWidth();
+                    if ((aX > obstacleTopLeftX && aX < obstacleTopRightX && aY > obstacleTopLeftY) || (bX > obstacleTopLeftX && bX < obstacleTopRightX && bY > obstacleTopLeftY)) {
 						collisionPresent = 1;
 						break;
 					}
-					obstacleGroupGap += 45;
-				}
-			} else { // 2 => 'flying' obstacle type
-				if (crouching == 0 && ((aX > obstacles[i] && aX < obstacles[i] + flyingObstacleWidth && aY > obstacleFlyingHeight) || (bX > obstacles[i] && bX < obstacles[i] + flyingObstacleWidth && bY > obstacleFlyingHeight))) {
+					drawingPointX += (obstacles[i].getObstacleWidth() + GroundObstacle.getGroupGap()); // evaluating the next obstacle in group and accounting for the 5px gap in between
+                }
+            } else { //'flying' obstacle type
+                let flyingObsWidth = obstacles[i].getObstacleWidth();
+				if (!crouching && ((aX > obstacles[i].getPosition() && aX < obstacles[i].getPosition() + flyingObsWidth && aY > obstacles[i].getFlyingHeight()) || (bX > obstacles[i].getPosition() && bX < obstacles[i].getPosition() + flyingObsWidth && bY > obstacles[i].getFlyingHeight()))) {
 					collisionPresent = 1;
 					break;
 				}
 			}
-		}
-	}
-	return collisionPresent;
+        }
+    }
+    return collisionPresent;
 }
+
+runGame();
