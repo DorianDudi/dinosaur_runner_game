9f9effb0c3eb34ddc532bdf7f030ca82d8306514
Dorian-DV <ddorianvlad@yahoo.ro>
2023-12-18 19:51:13 +0200

    Update drawing_scripts.js

--- drawing_scripts.js
+++ drawing_scripts.js
@@ -2,39 +2,22 @@ function drawGame(X, Y) {
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	ctx.fillStyle = 'green';
 	ctx.fillRect(X, Y, dinosaurWidth, dinosaurHeight);
-	/*
-	ctx.fillStyle = 'magenta'; //highlights collision points - for testing only
-	ctx.fillRect(X, Y + dinosaurHeight, 5, 5);
-	ctx.fillRect(X + dinosaurWidth, Y + dinosaurHeight, 5, 5);
-	*/
-	ctx.fillStyle = 'red'; //320 height
-	//ctx.fillRect(0, 320, 3350, 1);
+	ctx.fillStyle = 'red';
 	drawObstacles();
-	drawEndGameMessage();
+	if (gameOver) {
+		drawEndGameMessage();
+	}
 }
 
 function drawObstacles() {
-	for (let i = 0; i < obstacles.length; ++i) { //for every obstacle
+	for (let i = 0; i < obstacles.length; ++i) {
 		let obstacleGroupGap = 0;
 		ctx.fillStyle = 'red';
-		if (obstacleType[i] == 2) {
-			ctx.fillRect(obstacles[i], obstacleFlyingHeight, flyingObstacleWidth, heightSegment);
-			//highlights collision points - for testing only
-			/*
-			ctx.fillStyle = 'black';
-			ctx.fillRect(obstacles[i], obstacleFlyingHeight + heightSegment, 4, 4);
-			ctx.fillRect(obstacles[i]  + flyingObstacleWidth, obstacleFlyingHeight + heightSegment, 4, 4);
-			ctx.fillStyle = 'red';
-			*/
+		if (obstacles[i].getType() == "flying") {
+			ctx.fillRect(obstacles[i].getPosition(), obstacles[i].getFlyingHeight(), obstacles[i].getObstacleWidth(), obstacles[i].getHeightSegment());
 		} else {
-			for (let k = 0; k < groupMembersHeight[i].length; ++k) {
-				ctx.fillRect(obstacles[i] + obstacleGroupGap, obstacleBottom, obstacleWidth, -45 * groupMembersHeight[i][k]);
-				/*
-				ctx.fillStyle = 'black';
-				ctx.fillRect(obstacles[i] + obstacleGroupGap, obstacleTop + ((groupMembersHeight[i][k] - 1) * heightSegment), 4, 4);
-				ctx.fillRect(obstacles[i] + obstacleGroupGap  + obstacleWidth, obstacleTop + ((groupMembersHeight[i][k] - 1) * heightSegment), 4, 4);
-				ctx.fillStyle = 'red';
-				*/
+			for (let k = 0; k < obstacles[i].getElementsCount(); ++k) {
+				ctx.fillRect(obstacles[i].getPosition() + obstacleGroupGap, Obstacle.getObstacleBottom(), obstacles[i].getObstacleWidth(), obstacles[i].getHeightSegment() * obstacles[i].getElementsHeight(k));
 				obstacleGroupGap += 45;
 			}
 		}
@@ -42,9 +25,7 @@ function drawObstacles() {
 }
 
 function drawEndGameMessage() {
-	if (gameOver) {
-		ctx.fillStyle = 'black';
-		ctx.font = '20px sans-serif';
-		ctx.fillText('GAME OVER', canvasWidth/2 - 60, canvasHeight/2);
-	}
+	ctx.fillStyle = 'black';
+	ctx.font = '20px sans-serif';
+	ctx.fillText('GAME OVER', canvasWidth/2 - 60, canvasHeight/2);
 }
